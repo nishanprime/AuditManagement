23bef3147fea20a3da2deeb426d1f9a19a4b4749
nishanprime <thapanishan9@gmail.com>
2021-10-11 23:18:06 +0545

    auditor edit screen

--- admindash/src/App.js
+++ admindash/src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import LoginScreen from './screens/LoginScreen';
-import AdminPanel from './screens/AdminPanel';
+import LoginScreen from "./screens/LoginScreen";
+import AdminPanel from "./screens/AdminPanel";
+import AuditorEditScreen from "./screens/AuditorEditScreen";
 
 function App() {
   return (
     <>
       <Router>
-        <Route path="/login" component={LoginScreen} exact/>
-        <Route path="/dashboard" component={AdminPanel} exact/>
+        <Route path="/login" component={LoginScreen} exact />
+        <Route path="/dashboard" component={AdminPanel} />
+        <Route path="/master/auditor/:id/edit" component={AuditorEditScreen} />
+
       </Router>
-      </>
+    </>
   );
 }
 
