25c8fe43754b51d4799e9a69667fc5c5fb99e21b
nishanprime <thapanishan9@gmail.com>
2021-10-11 22:26:08 +0545

    Pre Table Editing

--- admindash/src/components/dashboard/Dashboard.js
+++ admindash/src/components/dashboard/Dashboard.js
@@ -6,27 +6,39 @@ import CardSection from "./CardSection";
 const Dashboard = ({ auditors }) => {
   console.log(auditors);
 
-  return auditors.map((a) => {
-    return (
-      <div>
-        <div className="content-wrapper">
-          <Breadcrumbs/>
-          {/* Main content */}
-          <section className="content">
-            <div className="container-fluid">
-              <CardSection/>
-              {/* Main row */}
-              <Table />
-              <div className="row"></div>
-              {/* /.row (main row) */}
-            </div>
-            {/* /.container-fluid */}
-          </section>
-          {/* /.content */}
-        </div>
+  return (
+    <div>
+      <div className="content-wrapper">
+        {/* Main content */}
+        <section className="content">
+          <div className="container-fluid">
+            <CardSection />
+            {/* Main row */}
+            <Table auditors={auditors} />
+            <div className="row"></div>
+            {/* /.row (main row) */}
+          </div>
+          {/* /.container-fluid */}
+        </section>
+        {/* /.content */}
       </div>
-    );
-  });
+    </div>
+
+    // <div>
+    //   {[1, 2, 3].map((a) => {
+    //     return (
+
+    //     );
+    //   })}
+    //   )
+    // </div>
+  );
 };
+
+// auditors.map((a) => {
+//   return (
+
+//   );
+// }
 
 export default Dashboard;
