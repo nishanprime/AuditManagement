fe03ca9f827d7e5d478d5b59d61980e8a044b5e8
bipin0x01 <bt.kaji@gmail.com>
2021-10-11 22:26:37 +0545

    Added Responsiveness to AuditorsList Table

--- admindash/src/components/dashboard/AuditorListTable.js
+++ admindash/src/components/dashboard/AuditorListTable.js
@@ -9,8 +9,8 @@ const Table = () => {
                 <h3 className="card-title">DataTable with default features</h3>
                 </div>
               {/* <!-- /.card-header --> */}
-              <div className="card-body">
-                <table id="example1" className="table table-bordered table-striped">
+          <div className="card-body" style={{ "overflow-x": "auto"}}>
+                <table id="example1" className="responsive table table-bordered table-striped ">
                   <thead>
                   <tr>
                     <th>Auditor ID</th>
