33950d01002672e1b481a05bcf35e520a166e60b
nishanprime <thapanishan9@gmail.com>
2021-10-28 13:41:14 +0545

    Auditors Screen Fixed

--- admindash/src/screens/Auditors.js
+++ admindash/src/screens/Auditors.js
@@ -1,38 +1,47 @@
-import React, { useEffect } from 'react';
-import Loader from '../components/Loader';
-import { useDispatch, useSelector } from 'react-redux';
-import { getClientDetailsAction } from '../actions/clientAction';
-import Table from '../components/dashboard/AuditorListTable';
-import Breadcrumbs from '../components/dashboard/Breadcrumbs';
-import Message from '../components/Message';
+import React, { useEffect } from "react";
+import Loader from "../components/Loader";
+import { useDispatch, useSelector } from "react-redux";
+import { getClientDetailsAction } from "../actions/clientAction";
+import Table from "../components/dashboard/AuditorListTable";
+import Breadcrumbs from "../components/dashboard/Breadcrumbs";
+import Message from "../components/Message";
 
-const Auditors = ({ auditors }) => {
-	const dispatch = useDispatch();
-	const auditorDelete = useSelector((state) => state.auditorDelete);
+const Auditors = () => {
+  const dispatch = useDispatch();
+  const auditorDelete = useSelector((state) => state.auditorDelete);
 
-	const {
-		loading: auditorDeleteLoading,
-		error: auditorDeleteError,
-		success: auditorDeleteSuccess,
-	} = auditorDelete;
+  const {
+    loading: auditorDeleteLoading,
+    error: auditorDeleteError,
+    success: auditorDeleteSuccess,
+  } = auditorDelete;
 
-	const clientDetails = useSelector((state) => state.clientDetails);
-	const { clients } = clientDetails;
+  const clientDetails = useSelector((state) => state.clientDetails);
+  const { clients } = clientDetails;
 
-	useEffect(() => {
-		if (!clients) {
-			dispatch(getClientDetailsAction());
-		}
-	}, [auditorDeleteSuccess, dispatch]);
+  const auditorsDetails = useSelector((state) => state.auditorsDetails);
+  const { loading, error, auditors } = auditorsDetails;
 
-	return (
-		<>
-			<div className="content-wrapper">
-				<Breadcrumbs page="Auditors" />
-				{auditors && <Table auditors={auditors} />}
-			</div>
-		</>
-	);
+  useEffect(() => {
+    if (!clients) {
+      dispatch(getClientDetailsAction());
+    }
+  }, [auditorDeleteSuccess, dispatch]);
+
+  return (
+    <>
+      <div className="content-wrapper">
+        <Breadcrumbs page="Auditors" />
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          auditors && <Table auditors={auditors} />
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Auditors;
